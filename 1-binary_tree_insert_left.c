c34581b67fd11313e5cdd3af833b02fa084d1d93
snavy <1337snavy@gmail.com>
2023-01-09 00:38:45 +0000

    add: ex01

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function
+ * @parent: *binary_tree_t
+ * @value: int
+ *
+ * Return: *binary_tree_t
+*/
+binary_tree_t	*binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t	*r;
+
+	if (parent == 0)
+		return (0);
+	r = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (r == 0)
+		return (0);
+	if (parent->left)
+		r->left = parent->left;
+	parent->left = r;
+	r->parent = parent;
+	r->right = 0;
+	r->left = 0;
+	r->n = value;
+	return (r);	
+}

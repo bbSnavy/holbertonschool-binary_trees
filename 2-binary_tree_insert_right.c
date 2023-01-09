e29df8ef89566414d18f2089aa9fd66d04f3062f
snavy <1337snavy@gmail.com>
2023-01-09 14:14:15 +0000

    refactor: files

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,8 +7,7 @@
  *
  * Return: pointer to the created node
 */
-
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t	*binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t	*new_node;
 

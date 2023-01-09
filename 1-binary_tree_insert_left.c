129d423138e0c1113cccfd96e757587fcfc5af97
ydroal <droal.yoko@gmail.com>
2023-01-09 05:55:13 -0800

    update: task1, add: task2,3

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,20 +7,28 @@
  *
  * Return: *binary_tree_t
 */
-binary_tree_t	*binary_tree_insert_left(binary_tree_t *parent, int value)
-{
-	binary_tree_t	*r;
 
-	if (parent == 0)
-		return (0);
-	r = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-	if (r == 0)
-		return (0);
-	if (parent->left)
-		r->left = parent->left;
-	parent->left = r;
-	r->parent = parent;
-	r->right = 0;
-	r->n = value;
-	return (r);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t	*new_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->right = NULL;
+	new_node->left = NULL;
+	new_node->n = value;
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
 }
